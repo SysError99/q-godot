8ad113f4e58bac51bbc97c569f995bfe7d68c592
SysError99 <17522480+SysError99@users.noreply.github.com>
2023-01-13 23:19:05 +0700

    Fix subscriber array of bind_query()

--- addons/godot_groups/godot_groups.gd
+++ addons/godot_groups/godot_groups.gd
@@ -61,9 +61,9 @@ func bind_query(group_name: String, component_names: Array, system: Object, shar
 	yield(tree, "idle_frame")
 	var query_name := get_query_name(group_name, component_names)
 	var iterator := get_iterator(query_name)
-	var new_subscriber := [[system, shared]]
+	var new_subscriber := [system, shared]
 	iterator.subscribers.push_back(new_subscriber)
-	build_query(group_name, query_name, component_names, iterator, new_subscriber)
+	build_query(group_name, query_name, component_names, iterator, [new_subscriber])
 
 
 # API
