01b6ee89eee23fe04ac5b1eae0c8a6189283aec4
SysError99 <syserror99@outlook.com>
2023-04-20 01:48:53 +0700

    Remove warning since it isn't significant in most cases

--- addons/q_godot/q_godot.gd
+++ addons/q_godot/q_godot.gd
@@ -77,11 +77,6 @@ class Query extends Object:
 		node.set_meta(_instance_id, binds)
 		bound_queries.push_back(self)
 		_nodes.push_back(binds)
-		if node.name in by_name:
-			printerr("'%s' already exists in the query '%s'!" % [node.name, "%s::[%s]" % [_parent_class_name, ",".join(_sub_node_paths)]])
-			printerr("Make sure there are no main nodes with same names exist.")
-			printerr("BEWARE, THIS WILL CAUSE CATASTROPHE LATER, PLEASE FIX!")
-			return
 		by_name[node.name] = binds
 
 
